38ecb4dc06c37faa7bbaf2bd7d16e5acf77a9e3f
mcdavid95 <mcdavidemereuwa95@gmail.com>
2018-08-18 23:45:41 +0100

    ch(image): change mushroom image from red mushroom to green mushroom

--- index.jsx
+++ index.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Game from './src/components/Game';
-import Consumer from './src/Consumer';
 import ErrorBoundary from './src/components/ErrorBoundary';
-import Provider from './src/Provider';
 
 const app = document.getElementById('root');
 
 render(
-    <Consumer>
-      <ErrorBoundary>
-        <Game/>
-      </ErrorBoundary>
-    </Consumer>,
+    <ErrorBoundary>
+      <Game/>
+    </ErrorBoundary>,
   app
 );
 // render(
